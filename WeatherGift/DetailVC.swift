1d0fcfa3f9df05664ba2eb56a064426d94212265
Ellana Lawrence <elawrence.blain@gmail.com>
2019-10-17 17:22:07 -0400

    7.2 Complete

---
+++ WeatherGift/DetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by Ellana Lawrence on 10/15/19.
+//  Copyright © 2019 Ellana Lawrence. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- WeatherGift/ListVC.swift
+++ WeatherGift/ListVC.swift
@@ -1,20 +1,20 @@
 //
-//  ViewController.swift
+//  ListVC.swift
 //  WeatherGift
 //
-//  Created by Ellana Lawrence on 10/15/19.
+//  Created by Ellana Lawrence on 10/17/19.
 //  Copyright © 2019 Ellana Lawrence. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
-
