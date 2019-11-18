3d02159803acf1c7f04d650cc84a55b0e7f66025
Ellana Lawrence <elawrence.blain@gmail.com>
2019-11-18 01:51:03 -0500

    7.15 Complete

---
+++ WeatherGift/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  WeatherGift
+//
+//  Created by Ellana Lawrence on 11/18/19.
+//  Copyright © 2019 Ellana Lawrence. All rights reserved.
+//
+
+import Foundation
+
+let urlBase = "https://api.darksky.net/forecast/"
+let urlAPIKey = "91f727a77474d1baa65ed1041ec68936/"
+
+
